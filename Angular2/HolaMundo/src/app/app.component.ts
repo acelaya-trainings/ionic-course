446aa8431503e94bdd8b02b85b467176ac0ac97d
AULA 4 <AULA 4>
2017-05-08 19:50:43 +0200

    Updated app component

--- Angular2/HolaMundo/src/app/app.component.ts
+++ Angular2/HolaMundo/src/app/app.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>Hello {{foo}}</h1>
+    <h2>{{bar}}</h2>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+  foo = 'World';
+  bar = 'acelaya';
+}
