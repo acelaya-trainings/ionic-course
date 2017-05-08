77733442272f82f940a92803663c5bd9e8dce573
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-05-08 21:37:37 +0200

    Created foter and header components

--- Angular2/HolaMundo/src/app/app.module.ts
+++ Angular2/HolaMundo/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { HeaderComponent } from "./header.component";
+import { FooterComponent } from "./footer.component";
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HeaderComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
