a4a9e6a0b994c32028ec2d535bd373e9e926d2d2
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-05-09 21:28:31 +0200

    Fixed indentation

--- Angular2/Products/src/app/products/products.component.ts
+++ Angular2/Products/src/app/products/products.component.ts
@@ -11,46 +11,46 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit() {
     this.products = [
-        {
+      {
         imageUrl: 'https://cdn4.iconfinder.com/data/icons/fineline-fill-gardening/128/garden_gardening_equipment_tool-19-256.png',
         name: 'Leaf Rake',
         code: '001',
         availableDate: '2016-01-01',
         price: 19.95,
         rating: 3
-        },
-        {
+      },
+      {
         imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51-QGor8qKL._SL256_.jpg',
         name: 'Garden Cart',
         code: '002',
         availableDate: '2016-01-02',
         price: 32.99,
         rating: 4
-        },
-        {
+      },
+      {
         imageUrl: 'http://www.pngall.com/wp-content/uploads/2016/04/Hammer-Download-PNG.png',
         name: 'Hammer',
         code: '003',
         availableDate: '2016-01-03',
         price: 8.99,
         rating: 5
-        },
-        {
+      },
+      {
         imageUrl: 'http://plainicon.com/download-icons/51042/plainicon.com-51042-aa67-256px.png',
         name: 'Saw',
         code: '004',
         availableDate: '2016-01-04',
         price: 11.55,
         rating: 4
-        },
-        {
+      },
+      {
         imageUrl: 'https://cdn1.iconfinder.com/data/icons/devices-7/128/devices-19-256.png',
         name: 'Video game controller',
         code: '005',
         availableDate: '2016-01-05',
         price: 35.95,
         rating: 5
-        },
+      },
     ]
   }
 }
