a108c514bcad06bcf1082d285b768796f4467812
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-05-09 18:43:55 +0200

    Added method overload

--- Typescript/library/app.ts
+++ Typescript/library/app.ts
@@ -23,6 +23,29 @@ export class BookManager {
     getBooksByCategory(category: Category = Category.Syfy): any[] {
         return this.books.filter(book => book.category === category);
     }
+
+    // Method overload
+    getTitlesBy(author: string): string[];
+    getTitlesBy(avail: boolean): string[];
+    getTitlesBy(param: any): string[] {
+        let titles: string[] = [];
+
+        if (typeof param === 'string') {
+            for (let book of this.books) {
+                if (book.author === param) {
+                    titles.push(book.title);
+                }
+            }
+        } else if (typeof param === 'boolean') {
+            for (let book of this.books) {
+                if (book.available === param) {
+                    titles.push(book.title);
+                }
+            }
+        }
+
+        return titles;
+    }
 }
 
 let manager = new BookManager();
@@ -31,3 +54,6 @@ const books = manager.getAllBooks();
 console.log(manager.getBooksByCategory(Category.History));
 console.log(manager.getBooksByCategory(Category.Poetry));
 console.log(manager.getBooksByCategory());
+console.log(manager.getTitlesBy('Someone'));
+console.log(manager.getTitlesBy(false));
+console.log(manager.getTitlesBy(true));
