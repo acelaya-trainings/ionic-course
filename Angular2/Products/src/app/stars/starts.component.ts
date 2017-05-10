1af4210234606a343c75e3e885e94e3465a1c4d0
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-05-10 21:10:01 +0200

    Improved stars component

--- Angular2/Products/src/app/stars/starts.component.ts
+++ Angular2/Products/src/app/stars/starts.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'stars',
   templateUrl: './stars.component.html',
+  styleUrls: ['./stars.component.css'],
 })
 export class StarsComponent  {
     @Input()
