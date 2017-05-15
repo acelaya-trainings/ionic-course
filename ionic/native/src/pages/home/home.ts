5fd6cca34e93b587a2954fee2de56f4284c19553
Alejandro Celaya <alejandro@alejandrocelaya.com>
2017-05-15 19:46:25 +0200

    Created ionic native project

---
+++ ionic/native/src/pages/home/home.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  imageData: string;
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  takePicture() {
+
+  }
+
+  showAlbum() {
+
+  }
+
+}
